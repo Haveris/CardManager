6837f8a21866df41ab03527463b2629030bdddb4
Patryk Banaszak <Patryk.Banaszak@student.ajp.edu.pl>
2022-06-08 19:37:50 +0200

    Update6 still working

--- app/src/main/java/com/example/cardmanager/AddingCardActivity.java
+++ app/src/main/java/com/example/cardmanager/AddingCardActivity.java
@@ -1,22 +1,37 @@
 package com.example.cardmanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class AddingCardActivity extends AppCompatActivity {
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     private Button button_frontCard, button_reverseCard;
+    ImageView imageView_frontCard, imageView_reverseCard;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +41,11 @@ public class AddingCardActivity extends AppCompatActivity {
 
 
         ///////////////////////////////////////////////////////////////////
+        imageView_frontCard = (ImageView) findViewById(R.id.imageView_frontCard);
+        imageView_reverseCard = (ImageView) findViewById(R.id.imageView_reverseCard);
+        ///////////////////////////////////////////////////////////////////
+
+
         button_frontCard = (Button) findViewById(R.id.button_frontCard);
         button_frontCard.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,21 +54,21 @@ public class AddingCardActivity extends AppCompatActivity {
         button_reverseCard = (Button) findViewById(R.id.button_reverseCard);
         button_reverseCard.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                dispatchTakePictureIntent(); }});
+            public void onClick(View v) {dispatchTakePictureIntent(); }});
         ///////////////////////////////////////////////////////////////////
+
+
+
     }
 
     //End onCreate method
 
     private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        try {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        } catch (ActivityNotFoundException e) {
-            // display error state to the user
-        }
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivity(intent);
     }
+
+
 
 
 }
